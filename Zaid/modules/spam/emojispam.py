256243e4982eb1cee85c738e01845b18a234d488
HKMUSIC <himanshu4hk9@gmail.com>
2025-09-03 20:09:57 +0530

    Update emojispam.py

--- Zaid/modules/spam/emojispam.py
+++ Zaid/modules/spam/emojispam.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import asyncio
+from Zaid import SUDO_USER
 
 @Client.on_message(
     filters.command(["spamemoji"], ".") & (filters.me | filters.user(SUDO_USER))
