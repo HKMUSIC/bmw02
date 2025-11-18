1de3c3a83291248c891215b4383906fc48cd424a
HKMUSIC <himanshu4hk9@gmail.com>
2025-11-19 00:54:38 +0530

    Update botban.py

--- Zaid/modules/basic/botban.py
+++ Zaid/modules/basic/botban.py
@@ -1,24 +1,34 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from Zaid.modules.basic.profile import extract_user
 from Zaid.database import botbandb as BotDB
 
-# MULTIPLE ADMINS
-BOT_ADMINS = [8323081123, 7553434931]  # yaha apne ids daalo
+BOT_ADMINS = [8323081123, 7553434931]
+
+def get_target_user(message: Message):
+    """username / user_id / reply se user laane ka simple method"""
+    if message.reply_to_message:
+        return message.reply_to_message.from_user.id
+    
+    if len(message.command) > 1:
+        return message.command[1]  # username or ID
+    
+    return None
+
 
 @Client.on_message(filters.command("botban") & filters.user(BOT_ADMINS))
 async def bot_ban_cmd(client: Client, message: Message):
-    user = await extract_user(message)
-    if not user:
-        return await message.reply("User not found!")
+
+    target = get_target_user(message)
+    if not target:
+        return await message.reply("❌ User not found! Username ya reply do.")
 
     try:
-        user = await client.get_users(user)
+        user = await client.get_users(target)
     except:
-        return await message.reply("Invalid user!")
+        return await message.reply("❌ Invalid user! Sahi username ya ID do.")
 
     if await BotDB.is_botbanned(user.id):
-        return await message.reply("User already bot banned!")
+        return await message.reply("⚠️ User already bot-banned!")
 
     await BotDB.botban_user(user.id)
     await message.reply(
@@ -26,21 +36,23 @@ async def bot_ban_cmd(client: Client, message: Message):
     )
 
 
+
 @Client.on_message(filters.command("botunban") & filters.user(BOT_ADMINS))
 async def bot_unban_cmd(client: Client, message: Message):
-    user = await extract_user(message)
-    if not user:
-        return await message.reply("User not found!")
+
+    target = get_target_user(message)
+    if not target:
+        return await message.reply("❌ User not found! Username ya reply do.")
 
     try:
-        user = await client.get_users(user)
+        user = await client.get_users(target)
     except:
-        return await message.reply("Invalid user!")
+        return await message.reply("❌ Invalid user! Sahi username ya ID do.")
 
     if not await BotDB.is_botbanned(user.id):
-        return await message.reply("User is not bot banned!")
+        return await message.reply("⚠️ User is NOT bot-banned!")
 
     await BotDB.botunban_user(user.id)
     await message.reply(
         f"✅ **Bot-Unbanned:** [{user.first_name}](tg://user?id={user.id})"
-    )
+        )
